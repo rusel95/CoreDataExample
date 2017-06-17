db94d322f319b1121196fd29de171e57bb6be4d9
rusel95 <ruslanpopesku95@gmail.com>
2017-06-17 14:48:24 +0300

    First commit

---
+++ CoreDataExample/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  CoreDataExample
+//
+//  Created by Admin on 17.06.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+extension Tweet {
+    
+}
